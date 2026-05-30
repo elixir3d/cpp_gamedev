eacc7dc04f2e55d5d00c14f8f9a0be6f0a50721f
elixir3d <mitch.gibbs@protonmail.com>
2026-05-30 13:44:23 +0100

    Changes to be committed: 	modified:   assignments/week01_mg_assignment.cpp     Notes : added a solution to the first assignment

--- assignments/week01_mg_assignment.cpp
+++ assignments/week01_mg_assignment.cpp
@@ -6,7 +6,74 @@
 #include <iostream>
 #include <vector>
 
+struct Node
+{
+    int val{0};
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+Node* createNode(int data);
+Node* insertIntoTree(Node* root, int val);
+void PrintTreeLeaves(Node* root);
+
 int main()
 {
+
+
+    Node* root = createNode(10);
+
+    root = insertIntoTree(root, 9);
+
+    root = insertIntoTree(root, 19);
+
+    root = insertIntoTree(root, 92);
+
+    root = insertIntoTree(root, 4);
+
+    root = insertIntoTree(root, 1);
+
+    root = insertIntoTree(root, 17);
+
+    PrintTreeLeaves(root);
+    
     return 0;
+}
+
+Node *createNode(int data)
+{
+    Node* newNode = new Node();
+    newNode->val = data;
+
+    return newNode;
+}
+
+Node* insertIntoTree(Node *root, int val)
+{
+    if (root == nullptr)  return createNode(val);
+
+    if (val < root->val)
+    {
+        root->left = insertIntoTree(root->left, val);
+
+    }
+    else if (val > root->val)
+    {
+        root->right = insertIntoTree(root->right, val);
+
+    }
+
+    return root;
+}
+
+void PrintTreeLeaves(Node *root)
+{
+    if (root == nullptr) return;
+    PrintTreeLeaves(root->left);  
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        std::cout << root->val << " ";
+    }
+    PrintTreeLeaves(root->right);
+
 }
